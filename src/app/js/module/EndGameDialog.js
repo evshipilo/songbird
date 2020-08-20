be5ee48a0beee5ab15c9b4e61de7f8606269792a
Yauheni Shypila <evshipilo@gmail.com>
2020-08-20 22:34:39 +0300

    feat: add end game dialog congratulations

--- src/app/js/module/EndGameDialog.js
+++ src/app/js/module/EndGameDialog.js
@@ -7,27 +7,28 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 
 export const EndGameDialog = observer(() => {
-  const store=useContext(StoreContext)
+  const store = useContext(StoreContext)
 
-  return(
+  const content = store.score === 30 ?
+    'Игра окончена. Вы набрали максимальное количество баллов! Поздравляю, вы отлично разбираетесь в рок-музыке!'
+    :
+    `Игра окончена. Вы набрали ${store.score} баллов из 30. Попробуйте ещё раз.`
+  return (
     <Dialog
       open={store.showModal}
-      onClose={()=>store.setShowModal(false)}
+      //onClose={() => store.setShowModal(false)}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          Let Google help apps determine location. This means sending anonymous location data to
-          Google, even when no apps are running.
-        </DialogContentText>
-      </DialogContent>
+      <DialogTitle id="alert-dialog-title">
+       {content}
+      </DialogTitle>
       <DialogActions>
-        <Button onClick={()=>store.nextGame()} color="primary" autoFocus>
-          NEW GAME
+        <Button onClick={() => store.nextGame()} color="primary" autoFocus>
+          НОВАЯ ИГРА
         </Button>
       </DialogActions>
     </Dialog>
